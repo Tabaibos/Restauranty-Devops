47d19786c2cdf27d15ed5abf2b70ee8bb8b629e3
Tabaibos <98jalmeida@gmail.com>
2026-09-10 00:43:30 +0100

    testing cosign on the rest of pipeline

--- backend/discounts/app.js
+++ backend/discounts/app.js
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 app.use(httpMetricsMiddleware);
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware. added comment
 require("./config")(app);
 
 require('./metrics.cjs');
